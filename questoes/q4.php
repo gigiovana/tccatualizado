8e285779d042cbed392cd642c265781315a2787a
Aluno <eullerifpr@gmail.com>
2018-11-28 14:49:08 -0300

    arrastar 4

--- questoes/q4.php
+++ questoes/q4.php
@@ -8,13 +8,15 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Handport - Questão 4</title>
+  <title>Handport - Modelo</title>
 
   <!-- Bootstrap core CSS -->
   <link href="../css/bootstrap.css" rel="stylesheet">
 
   <!-- Importar o Font awesome -->
   <link href="../css/font-awesome.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="../css/jquery-ui.min.css">
 
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
@@ -27,16 +29,16 @@
 
 <body>
 
-  <?php
-  include("../site/menu.php");
-   ?>
+    <?php
+    include("../site/menu.php");
+     ?>
 
     <!-- Início do cabeçalho -->
     <header class="masthead text-center text-white">
       <div class="masthead-content">
         <div class="container">
-          <h1 class="masthead-heading mb-0">Questão 4</h1>
-        <!--  <h2 class="masthead-subheading mb-0">ou divulgue aqui se você quer doar algum!</h2> -->
+          <h1 class="masthead-heading mt-3 mb-0">Modelo</h1>
+          <!-- <h2 class="masthead-subheading mb-0">Algum texto introdutório pra essa questão! Se não quiser, comente esta linha</h2> -->
         </div>
       </div>
       <div class="bg-circle-1 bg-circle"></div>
@@ -48,47 +50,113 @@
 
     <!-- Início do conteúdo -->
     <section>
-      <div class="container">
-        <div class="row align-items-center">
+      <div class="container-fluid py-4" >
+        <div class="row">
           <div class="col-lg-6">
-            <div class="p-5">
-
-              <!-- colocar video aqui -->
-              <img class="img-fluid rounded-circle" src="../img/01.jpg" alt="">
-
+            <div class="p-3 text-center">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Uhy3gtZoeOM" allowfullscreen></iframe>
+              </div>
             </div>
           </div>
           <div class="col-lg-6">
-            <div class="p-5">
-        <p class="lead">4- Ordene as frases, colocando na ordem: o elemento que realiza a ação, em seguida, o verbo e depois seu complemento.</p>
+            <div class="p-3">
+              <h2>Modelo</h2>
+              <!-- <p class="lead">Aqui vai o conteúdo da sua questão. Seguem alguns exemplos de código...</p> -->
+              <p class="lead">Ordene as frases, colocando na ordem: o elemento que realiza a ação, em seguida, o verbo e depois seu complemento.</p>
 
-        <div class="f1">
-          <br>
-          <br>
-          <span id="letra">A)</span>
-          <span class="q1a border rounded p-2 m-2" draggable="true" ondragstart="drag(event)" id="caixa1" width="88" height="31" >A bola</span>
-          <span class="q1a border rounded p-2 m-2" draggable="true" ondragstart="drag(event)" id="caixa2" width="88" height="31" >Jogou</span>
-          <span class="q1a border rounded p-2 m-2" draggable="true" ondragstart="drag(event)" id="caixa3" width="88" height="31" >O menino</span>
-          <div class="limparfloat"></div>
-        </div>
 
-        <br>
 
-        <div class="resp1">
-          <div class="r1 q1a border rounded p-4 m-3 float-left w-25" ondrop="drop(event)" ondragover="allowDrop(event)">  </div>
-          <div class="r1 q1a border rounded p-4 m-3 float-left w-25" ondrop="drop(event)" ondragover="allowDrop(event)">  </div>
-          <div class="r1 q1a border rounded p-4 m-3 float-left w-25" ondrop="drop(event)" ondragover="allowDrop(event)">  </div>
-          <div class="limparfloat"></div>
-        </div>
+              <div class="row my-3">
 
-      </div>
+                <div class="col-lg">
 
-    </div>
+                  <div class="bg-light rounded border p-3 text-center">
+
+
+                    <span id="letra">A)</span>
+                    <span class="q1a border rounded p-2 m-2 caixaPalavraA" id="caixa1" width="88" height="31" >A bola</span>
+                    <span class="q1a border rounded p-2 m-2 caixaPalavraA" id="caixa2" width="88" height="31" >Jogou</span>
+                    <span class="q1a border rounded p-2 m-2 caixaPalavraA" id="caixa3" width="88" height="31" >O menino</span>
+                    <div class="limparfloat"></div>
+                  </div>
+
+
+                  <div class="resp1">
+                    <div class="r1 q1a border rounded p-4 m-3 float-left w-25 caixaSoltarA text-center align-middle"> </div>
+                    <div class="r1 q1a border rounded p-4 m-3 float-left w-25 caixaSoltarA text-center align-middle"> </div>
+                    <div class="r1 q1a border rounded p-4 m-3 float-left w-25 caixaSoltarA text-center align-middle">  </div>
+                    <div class="limparfloat"></div>
+                  </div>
+                  <br>
+                  <br>
+
+                  <br>
+                  <br>
+
+                  <div class="bg-light rounded border p-3 text-center">
+
+                    <span id="letra">B)</span>
+                    <span class="q1b border rounded p-2 m-2 caixaPalavraB" id="caixa4" width="88" height="31" >Os meninos</span>
+                    <span class="q1b border rounded p-2 m-2 caixaPalavraB" id="caixa5" width="88" height="31" >Seus tênis</span>
+                    <span class="q1b border rounded p-2 m-2 caixaPalavraB" id="caixa6" width="88" height="31" >Venderam</span>
+                    <div class="limparfloat"></div>
+                  </div>
+
+                    <div class="resp2">
+                    <div class="r2 q1b border rounded p-4 m-3 float-left w-25 caixaSoltarB text-center align-middle"> </div>
+                    <div class="r2 q1b border rounded p-4 m-3 float-left w-25 caixaSoltarB text-center align-middle"> </div>
+                    <div class="r2 q1b border rounded p-4 m-3 float-left w-25 caixaSoltarB text-center align-middle">  </div>
+                    <div class="limparfloat"></div>
+                  </div>
+                  <br>
+                  <br>
+
+                  <br>
+                  <br>
+
+
+                  <div class="bg-light rounded border p-3 text-center">
+
+                    <span id="letra">C)</span>
+                    <span class="q1c border rounded p-2 m-2 caixaPalavraC" id="caixa7" width="88" height="31" >Gostamos</span>
+                    <span class="q1c border rounded p-2 m-2 caixaPalavraC" id="caixa8" width="88" height="31" >Eu e minha família</span>
+                    <span class="q1c border rounded p-2 m-2 caixaPalavraC" id="caixa9" width="88" height="31" >De lasanha</span>
+                    <div class="limparfloat"></div>
+                    </div>
+
+                    <div class="resp3">
+                      <div class="r3 q1c border rounded p-4 m-3 float-left w-25 caixaSoltarC text-center align-middle"> </div>
+                      <div class="r3 q1c border rounded p-4 m-3 float-left w-25 caixaSoltarC text-center align-middle">  </div>
+                      <div class="r3 q1c border rounded p-4 m-3 float-left w-25 caixaSoltarC text-center align-middle">  </div>
+                      <div class="limparfloat"></div>
+                    </div>
+                    <br>
+                    <br>
+
+                    <br>
+                    <br>
+
+                      </div>
+                </div>
+
+                  </div>
+                </div>
+
               </div>
 
+
+            </div>
+
+            <div class="p-3 text-right">
+              <button type="button" name="button" class="btn btn-success btn-lg px-3">
+                Enviar
+                <i class="fa fa-arrow-circle-right ml-2" aria-hidden="true"></i>
+              </button>
             </div>
           </div>
-
+        </div>
+      </div>
     </section>
     <!-- fim do conteúdo -->
 
@@ -107,15 +175,53 @@
     <script src="../js/popper.js"></script>
     <script src="../js/bootstrap.js"></script>
 
-    <!-- Só existe para garantir que o bloco js vai ser executado
-    depois de importar as bibliotecas -->
+     <script src="../js/jquery-ui.min.js"></script>
+
     <script type="text/javascript">
     $(document).ready(function() {
 
+      // Aqui você define a classe das peças que podem ser arrastadas
+      $( ".caixaPalavraA, .caixaPalavraB, .caixaPalavraC " ).draggable({ revert: "invalid" });
+
+      // Definir a classe dos lugares em que as peças podem ser soltas
+      $(".caixaSoltarA").droppable({
+        // Definir a classe que pode ser solta Aqui
+        accept: ".caixaPalavraA",
+        // O que vai acontecer quando soltar a peça...
+        drop: function( event, ui ) {
+          // Aqui o que acontece quando solta ele...
+          // $( this ).html( "Dropped!" );
+        }
+      }); // fim do droppable
+
+      // Definir a classe dos lugares em que as peças podem ser soltas
+      $(".caixaSoltarB").droppable({
+        // Definir a classe que pode ser solta Aqui
+        accept: ".caixaPalavraB",
+        // O que vai acontecer quando soltar a peça...
+        drop: function( event, ui ) {
+          // Aqui o que acontece quando solta ele...
+          // $( this ).html( "Dropped!" );
+        }
+      }); // fim do droppable
+
+      // Definir a classe dos lugares em que as peças podem ser soltas
+      $(".caixaSoltarC").droppable({
+        // Definir a classe que pode ser solta Aqui
+        accept: ".caixaPalavraC",
+        // O que vai acontecer quando soltar a peça...
+        drop: function( event, ui ) {
+          // Aqui o que acontece quando solta ele...
+          // $( this ).html( "Dropped!" );
+        }
+      }); // fim do droppable
 
 
-    });
+
+    }); // fim do document ready
     </script>
+
+
 
 
   </body>
